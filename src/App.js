d882a8a129549c6291628462789332233c1def16
avhis-hek7 <shrmaavishek679@gmail.com>
2025-02-08 15:08:36 +0545

    background add

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import NavBar from './Component/NavBar';
 function App() {
   return (
     <div>
+        <div>
       <NavBar/>
     </div>
+    <div className="bg"></div>
+    </div>
+     
+   
     
   );
 }
